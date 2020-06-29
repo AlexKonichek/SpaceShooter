08e895226663d60288ec992af78ac145d9953f37
Alex Konichek <Konichekalex@gmail.com>
2020-06-29 21:33:51 +0300

    recut bg image for beowser height

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 let config = {
     type: Phaser.AUTO,
     width: 1920,
-    height: 1080,
+    height: 830,
     scene: [BootScene, PreloadScene, StartScene, GameScene],
     physics:{
         default:'arcade',
