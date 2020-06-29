a2ca1367b8964ae06f2f54221cc6286c3143e079
Alex Konichek <Konichekalex@gmail.com>
2020-06-29 20:50:02 +0300

    change bg size

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 let config = {
     type: Phaser.AUTO,
-    width: 617,
-    height: 347,
+    width: 1920,
+    height: 1080,
     scene: [BootScene, PreloadScene, StartScene, GameScene],
     physics:{
         default:'arcade',
